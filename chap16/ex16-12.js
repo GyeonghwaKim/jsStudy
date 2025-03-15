1759e5dfc7dd51fa4199fdf7c8c56827e6a2eb84
GyeongHwa <gyeonghwaa_@naver.com>
2025-03-15 14:49:36 +0900

    chap16(1)

---
+++ chap16/ex16-12.js
@@ -0,0 +1,19 @@
+const person = {name: 'Lee'}
+
+console.log(Object.isFrozen(person));
+
+Object.freeze(person);
+
+person.age = 20;
+console.log(person);
+
+delete person.naem;
+console.log(person);
+
+person.name = 'kim';
+console.log(person);
+
+
+Object.defineProperty(person, 'name', {
+    configurable: true
+});
